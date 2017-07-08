80c5cc4c37371732f5353b486870d1526e5c99e0
Zeeshan Ali <zeenix@gmail.com>
2017-07-08 23:20:14 +0200

    Make it build

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate rbpf;
 
 fn main() {
     // This is the eBPF program, in the form of bytecode instructions.
-    let prog = &[
+    let prog = vec![
         0xb4, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // mov32 r0, 0
         0xb4, 0x01, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, // mov32 r1, 2
         0x04, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, // add32 r0, 1
@@ -13,7 +13,7 @@ fn main() {
     // Instantiate a struct EbpfVmNoData. This is an eBPF VM for programs that
     // takes no packet data in argument.
     // The eBPF program is passed to the constructor.
-    let vm = rbpf::EbpfVmNoData::new(prog);
+    let vm = rbpf::EbpfVmNoData::new(& prog);
 
     // Execute (interpret) the program. No argument required for this VM.
     assert_eq!(vm.prog_exec(), 0x3);
