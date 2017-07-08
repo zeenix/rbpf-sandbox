9a5286f70905609709d9a890f4a12efc617d6fc5
Zeeshan Ali <zeenix@gmail.com>
2017-07-08 23:19:46 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate rbpf;
+
+fn main() {
+    // This is the eBPF program, in the form of bytecode instructions.
+    let prog = &[
+        0xb4, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // mov32 r0, 0
+        0xb4, 0x01, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, // mov32 r1, 2
+        0x04, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, // add32 r0, 1
+        0x0c, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // add32 r0, r1
+        0x95, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  // exit
+    ];
+
+    // Instantiate a struct EbpfVmNoData. This is an eBPF VM for programs that
+    // takes no packet data in argument.
+    // The eBPF program is passed to the constructor.
+    let vm = rbpf::EbpfVmNoData::new(prog);
+
+    // Execute (interpret) the program. No argument required for this VM.
+    assert_eq!(vm.prog_exec(), 0x3);
+}
